5d096aee901276fbef77ab04ae721d8a34dee139
Blaž Vrhovšek <blaz@genialis.com>
2021-03-29 19:09:09 +0200

    Fixed volums on Windows

--- Run_qtlspyer_on_windows10.bat
+++ Run_qtlspyer_on_windows10.bat
@@ -16,7 +16,7 @@ docker cp qtl_lamb:/QTLspyer/ .
 docker kill qtl_lamb
 
 echo "Starting QTLspyer"
-docker run -d --rm --init -p 3838:3838 --name qtl_spyer -v %~dp0\QTLspyer\:\QTLspyer hudogriz/qtl_spyer:latest
+docker run -d --rm --init -p 3838:3838 --name qtl_spyer -v %~dp0/QTLspyer/:/QTLspyer hudogriz/qtl_spyer:latest
 
 start "" http://localhost:3838
 
